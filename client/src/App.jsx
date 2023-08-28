6f4b43c9b01a914b97ff3b35e9be7331223c19c7
Saekyul <saekyulyang@gmail.com>
2023-08-28 15:50:42 +0900

    ✨ Tailwind 초기 세팅

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
