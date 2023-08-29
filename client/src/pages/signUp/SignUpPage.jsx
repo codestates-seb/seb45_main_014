bd3fa95f2802b49e8ba04142796d0c266037dd6f
Saekyul <saekyulyang@gmail.com>
2023-08-29 13:51:04 +0900

    ✨ 각 항목에 대한 유효성 검사

--- client/src/pages/signUp/SignUpPage.jsx
+++ client/src/pages/signUp/SignUpPage.jsx
@@ -1,9 +1,12 @@
 import images from '../../assets/images/Images';
 import { styled } from 'styled-components';
 import Button from '../../components/Button.jsx';
+import { userFormStore } from '../../store/store';
+import { useEffect } from 'react';
+import { getByteSize } from '../../utils/getByteSize';
 
 const Input = styled.input`
-  width: 20rem;
+  width: 30rem;
   height: 3rem;
   margin-top: 0.75rem;
   padding: 0.75rem;
@@ -16,6 +19,63 @@ const InputBox = styled.div`
 `;
 
 const SignUpPage = () => {
+  const {
+    email,
+    password,
+    confirmPassword,
+    nickname,
+    phone,
+    setErrors,
+    errors,
+    setEmail,
+    setPassword,
+    setConfirmPassword,
+    setNickname,
+    setPhone,
+  } = userFormStore();
+
+  useEffect(() => {
+    let newErrors = {};
+
+    // 이메일 유효성 검사
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (email.length > 30 || !emailPattern.test(email)) {
+      newErrors.email = '유효한 이메일을 입력해주세요.';
+    }
+
+    // 비밀번호 유효성 검사
+    const passwordPattern =
+      /^(?=.*[a-zA-Z])(?=.*[!@#$%^+=-])(?=.*[0-9]).{8,25}$/;
+    if (!passwordPattern.test(password)) {
+      newErrors.password =
+        '비밀번호는 8자 이상이면서 영문, 숫자, 특수문자를 포함해야 합니다.';
+    }
+
+    // 비밀번호 확인 유효성 검사
+    if (password !== confirmPassword) {
+      newErrors.confirmPassword = '비밀번호가 일치하지 않습니다.';
+    }
+
+    // 닉네임 유효성 검사
+    const nicknamePattern = /^[가-힣a-zA-Z]+$/;
+    if (
+      !nicknamePattern.test(nickname) ||
+      getByteSize(nickname) > 12 ||
+      getByteSize(nickname) < 4
+    ) {
+      newErrors.nickname = '닉네임은 한글 6자, 영문 12자까지 가능합니다.';
+    }
+
+    // 전화번호 유효성 검사
+    const cleanPhone = phone.replace(/-/g, '');
+    if (!/^\d{10,11}$/.test(cleanPhone) || cleanPhone.length > 11) {
+      newErrors.phone = '유효한 전화번호를 입력해주세요.';
+    }
+
+    setErrors(newErrors);
+  }, [email, password, confirmPassword, nickname, phone]);
+
   return (
     <div className="flex flex-col items-center">
       <img
@@ -28,60 +88,70 @@ const SignUpPage = () => {
         <form className="flex flex-col items-center">
           <InputBox>
             <label htmlFor="email" className="block">
-              이메일을 입력하세요
+              이메일
             </label>
             <Input
               type="email"
               id="email"
               placeholder="example@gmail.com"
               required=""
+              onChange={(e) => setEmail(e.target.value)}
             />
-            {/* <Button>중복 확인</Button> */}
           </InputBox>
+          <span className="text-red-500">{errors.email}</span>
           <InputBox>
             <label htmlFor="password" className="block">
-              비밀번호를 입력하세요
+              비밀번호
             </label>
             <Input
               type="password"
               id="password"
-              placeholder="********"
+              placeholder="••••••••"
               required=""
+              onChange={(e) => setPassword(e.target.value)}
             />
           </InputBox>
+          <span className="text-red-500">{errors.password}</span>
           <InputBox>
             <label htmlFor="password-confirm" className="block">
-              비밀번호를 다시 입력하세요
+              비밀번호 확인
             </label>
             <Input
               type="password"
               id="password-confirm"
-              placeholder="********"
+              placeholder="••••••••"
               required=""
+              onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </InputBox>
+          <span className="text-red-500">{errors.confirmPassword}</span>
           <InputBox>
             <label htmlFor="nickname" className="block">
-              닉네임을 입력하세요
+              닉네임
             </label>
             <Input
               type="text"
               id="nickname"
               placeholder="nickname"
               required=""
+              onChange={(e) => setNickname(e.target.value)}
             />
           </InputBox>
+          <span className="text-red-500">{errors.nickname}</span>
           <InputBox>
             <label htmlFor="phone" className="block">
-              전화번호를 입력하세요
+              전화번호
             </label>
             <Input
               type="tel"
               id="phone"
-              placeholder="010-1234-5678"
+              placeholder="01012345678 / - 없이 입력하세요"
               required=""
+              onChange={(e) => setPhone(e.target.value)}
             />
           </InputBox>
+          <span className="text-red-500">{errors.phone}</span>
+          <Button className="mt-5">회원가입</Button>
         </form>
       </section>
     </div>
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -4,3 +4,18 @@ export const useBookmarkStore = create((set) => ({
   isBookmarked: false,
   toggleBookmark: () => set((state) => ({ isBookmarked: !state.isBookmarked })),
 }));
+
+export const userFormStore = create((set) => ({
+  email: '',
+  password: '',
+  confirmPassword: '',
+  nickname: '',
+  phone: '',
+  errors: {},
+  setEmail: (email) => set({ email }),
+  setPassword: (password) => set({ password }),
+  setConfirmPassword: (confirmPassword) => set({ confirmPassword }),
+  setNickname: (nickname) => set({ nickname }),
+  setPhone: (phone) => set({ phone }),
+  setErrors: (errors) => set({ errors }),
+}));
