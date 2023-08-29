8ff91b00d4477d2fdc21796120b99514cbe8d08b
Saekyul <saekyulyang@gmail.com>
2023-08-29 16:20:00 +0900

    ✨ 리뷰 디테일 레이아웃

--- client/src/pages/myPage/Reviews.jsx
+++ client/src/pages/myPage/Reviews.jsx
@@ -2,11 +2,11 @@ import { styled } from 'styled-components';
 import images from '../../assets/images/Images';
 
 const ReviewDetailStyle = styled.div`
-  margin: 10px 0;
   width: 100%;
-  border-top: 1px solid #ccc;
   border-bottom: 1px solid #ccc;
   padding: 10px;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Stars = ({ rating }) => {
@@ -28,10 +28,15 @@ const Stars = ({ rating }) => {
 const ReviewDetail = () => {
   return (
     <ReviewDetailStyle>
-      <h2>{'맛있는 빵집 1004호점'}</h2>
-      <div className="flex">
-        <Stars rating={3} />
+      <div>
+        <h2>{'맛있는 빵집 1004호점'}</h2>
+        <div className="flex">
+          <Stars rating={3} />
+          {'2023-08-20'}
+        </div>
+        <div className="mt-2">너무 맛있어용!</div>
       </div>
+      <div>이미지가 들어갈 곳입니다....</div>
     </ReviewDetailStyle>
   );
 };
@@ -40,6 +45,8 @@ const Reviews = () => {
   return (
     <div className="w-full">
       <h1>내가 쓴 리뷰 : {0} 개</h1>
+      <ReviewDetail />
+      <ReviewDetail />
       <ReviewDetail />
     </div>
   );
