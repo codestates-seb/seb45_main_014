db42f600c85e5febb80921893d3183c107fa0844
nuyhv <volumefd@gmail.com>
2023-08-29 10:56:23 +0900

    ✨ Header 초기 구성

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
 
 function App() {
   return (
     <>
+      <Header />
       <Footer />
     </>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './assets/styles/GlobalStyle.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+    <GlobalStyle />
     <App />
   </BrowserRouter>,
 );
