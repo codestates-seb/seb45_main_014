bf3dafd7880c2975292b8d3757eca7b2eca2500c
Saekyul <saekyulyang@gmail.com>
2023-08-30 13:58:50 +0900

    ✨ 배포 최적화 / Loading 컴포넌트 구현

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css';
-import Shop from './pages/Shop.jsx';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
-import MainPage from './pages/main/MainPage.jsx';
 import { createGlobalStyle } from 'styled-components';
 import GmarketSans from './assets/fonts/GmarketSansTTFMedium.ttf';
 import { Route, Routes } from 'react-router-dom';
-import SignUpPage from './pages/signUp/SignUpPage.jsx';
-import LoginPage from './pages/login/LoginPage.jsx';
-import MyPage from './pages/myPage/MyPage.jsx';
-import ReviewPost from './pages/myPage/ReviewPost.jsx';
+import { lazy, Suspense } from 'react';
+import LoadingSpinner from './components/Loading.jsx';
+
+const MainPage = lazy(() => import('./pages/main/MainPage.jsx'));
+const LoginPage = lazy(() => import('./pages/login/LoginPage.jsx'));
+const SignUpPage = lazy(() => import('./pages/signUp/SignUpPage.jsx'));
+const MyPage = lazy(() => import('./pages/myPage/MyPage.jsx'));
+const Shop = lazy(() => import('./pages/Shop.jsx'));
+const PostReview = lazy(() => import('./pages/myPage/PostReview.jsx'));
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
@@ -29,14 +32,17 @@ function App() {
       <GlobalStyle />
       <Header />
       <main className="max-w-screen-lg mx-auto pt-32 mb-64">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/reviews/post" element={<ReviewPost />} />
-        </Routes>
+        <Suspense fallback={<LoadingSpinner />}>
+          {/* <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/reviews/post" element={<PostReview />} />
+          </Routes> */}
+          <LoadingSpinner />
+        </Suspense>
       </main>
       <Footer />
     </>
