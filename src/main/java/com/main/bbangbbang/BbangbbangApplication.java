dc8a1d6d234a724f4adebc2e2ad57f95d636a379
tsulocalize <tsulocalizing@gmail.com>
2023-08-30 14:28:14 +0900

    ✨SpringBootApplication 생성

---
+++ src/main/java/com/main/bbangbbang/BbangbbangApplication.java
@@ -0,0 +1,13 @@
+package com.main.bbangbbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbangbbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbangbbangApplication.class, args);
+	}
+
+}
