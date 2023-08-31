8da4e6486731540aab987c62fdbf08c5b44d8fd6
Saekyul <saekyulyang@gmail.com>
2023-08-31 18:18:00 +0900

    ✨ 임시 슬라이드 기능 구현

--- client/src/assets/StoreCard.jsx
+++ client/src/assets/StoreCard.jsx
@@ -1,7 +1,7 @@
 import { useBookmarkStore } from '../store/store';
 import images from './images/Images';
 import { StoreImage } from './Styles.jsx';
-import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const BookmarkButton = ({ is_favorite, toggleBookmark }) => {
   return (
@@ -25,39 +25,36 @@ const StoreCard = ({ store }) => {
 
   return (
     <div className="w-72 relative m-5">
-      <StoreImage src={img} alt="매장 대표 이미지" />
-      <button
-        className="absolute bottom-16 right-1 p-2"
-        onClick={toggleBookmark}
-      >
-        <img
-          src={is_favorite ? images.bookmarkOn : images.bookmarkOff}
-          alt="북마크 아이콘"
-          width={24}
-          height={24}
-        />
-      </button>
-      <div className="flex justify-between">
-        <div className="flex flex-col">
-          <div className="flex">
-            <h2>{store_name}</h2>
-            <h2 className="ml-2 text-yellow-500">{formattedStoreRating}</h2>
+      <Link to={`/store/${store.id}`}>
+        <StoreImage src={img} alt="매장 대표 이미지" className="rounded-lg" />
+      </Link>
+      <BookmarkButton
+        is_favorite={is_favorite}
+        toggleBookmark={() => toggleBookmark(store.id)}
+      />
+      <Link to={`/store/${store.id}`}>
+        <div className="flex justify-between">
+          <div className="flex flex-col">
+            <div className="flex">
+              <h2>{store_name}</h2>
+              <h2 className="ml-2 text-yellow-500">{formattedStoreRating}</h2>
+            </div>
+            <div>{region_name}</div>
           </div>
-          <div>{region_name}</div>
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 48 48"
+              id="eye"
+              className="flex-shrink-0 rounded-full"
+              width="24"
+              height="24"
+            >
+              <path d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6z"></path>
+            </svg>
+          </div>
         </div>
-        <div className="flex">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
-            id="eye"
-            className="flex-shrink-0"
-            width="24"
-            height="24"
-          >
-            <path d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6z"></path>
-          </svg>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 };
--- client/src/pages/main/MainPage.jsx
+++ client/src/pages/main/MainPage.jsx
@@ -1,25 +1,58 @@
+import { styled } from 'styled-components';
 import StoreCard from '../../assets/StoreCard.jsx';
-import images from '../../assets/images/Images.js';
 import storeData from '../../assets/data/storeData';
+import { useState, useEffect, useRef } from 'react';
+
+const FavoriteStoreCard = ({ store, index }) => {
+  return (
+    <>
+      <div key={index}>
+        <a href={`/store/${store.id}`}>
+          <img src={store.img} alt="즐겨찾기된 매장" />
+        </a>
+        <div>{store.store_name}</div>
+      </div>
+    </>
+  );
+};
+
+const BannerItem = styled.div`
+  transition: opacity 1s ease-in-out;
+  opacity: ${(props) => (props.active ? '1' : '0')};
+`;
 
 const MainPage = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
   const favoriteStores = storeData.filter(
     (store) => store.is_favorite === true,
   );
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % favoriteStores.length);
+    }, 3000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [favoriteStores.length]);
+
   return (
-    <div className="max-w-screen-lg mx-auto">
+    <div className="">
       {/* 즐겨찾기된 매장 */}
-      {/* 즐겨찾기 모음 중 랜덤한 4개를 렌더링 할 것이냐? */}
       <div className="flex justify-center mx-5">
         <h1 className="flex  justify-center items-center">즐겨찾기</h1>
       </div>
       <div className="flex flex-col items-center overflow-x-auto whitespace-nowrap">
         <div className="flex">
           {favoriteStores.map((store, index) => (
-            <StoreCard key={index} store={store} />
+            <BannerItem key={index} active={index === currentIndex}>
+              <FavoriteStoreCard store={store} key={index} />
+            </BannerItem>
           ))}
+          {/* 더보기 버튼 임시 주석처리
           <div className="flex flex-col items-center justify-center hover:bg-gray-200 w-40">
+           
             <a href="/mypage#favorites">
               <img
                 src={images.arrow}
@@ -28,16 +61,17 @@ const MainPage = () => {
                 height={'70px'}
               />
             </a>
-            <div>더보기</div>
-          </div>
+            <div>더보기</div> 
+          </div> */}
         </div>
       </div>
-
-      <h1 className="flex justify-center">빵집 리스트</h1>
-      <div className="flex flex-wrap justify-center">
-        {storeData.map((store, index) => (
-          <StoreCard key={index} store={store} />
-        ))}
+      <div className="flex flex-col max-w-screen-md mx-auto">
+        <h1 className="flex justify-center">빵집 리스트</h1>
+        <div className="flex flex-wrap justify-center">
+          {storeData.map((store, index) => (
+            <StoreCard key={index} store={store} />
+          ))}
+        </div>
       </div>
     </div>
   );
