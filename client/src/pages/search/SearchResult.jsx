5d25b4b9021c1a3834053efd36c59e0833e570c5
nuyhv <volumefd@gmail.com>
2023-08-31 18:02:43 +0900

    ✅ 검색결과 더미데이터 연결 테스트

--- client/src/pages/search/SearchResult.jsx
+++ client/src/pages/search/SearchResult.jsx
@@ -1,7 +1,45 @@
 import { styled } from 'styled-components';
+import storeData from '../../assets/data/storeData';
+import { useState, useEffect } from 'react';
+import StoreCard from '../../assets/StoreCard.jsx';
+
+const Wrapper = styled.div`
+  max-width: 800px;
+  margin: 0 auto 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const DataContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
 
 const SearchResult = () => {
-  return <div>검색 결과</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(storeData);
+  }, []);
+
+  return (
+    <Wrapper>
+      검색 결과
+      <DataContainer>
+        {data.map((item) => (
+          <StoreCard
+            key={item.id}
+            storeName={item.store_name}
+            storeLocation={item.region_name}
+            storeRating={item.rating}
+          />
+        ))}
+      </DataContainer>
+    </Wrapper>
+  );
 };
 
 export default SearchResult;
