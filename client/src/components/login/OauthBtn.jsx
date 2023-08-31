445e8fcdce52cba87ea25df5becd8fc730de178a
Saekyul <saekyulyang@gmail.com>
2023-08-31 11:38:02 +0900

    💄 OAuth 버튼 그림자 살짝 연하게

--- client/src/components/login/OauthBtn.jsx
+++ client/src/components/login/OauthBtn.jsx
@@ -12,7 +12,7 @@ const OauthBtn = ({ bgColor, color, icon, text, onClick }) => {
         width: '300px',
         height: '40px',
         borderRadius: '8px',
-        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.5)',
+        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.2)',
         margin: '10px 0px',
         backgroundColor: bgColor,
         color: color,
