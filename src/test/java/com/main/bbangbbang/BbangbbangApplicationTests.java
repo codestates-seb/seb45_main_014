3f50e3f7005348f54d23a9835516c627a7ae086e
tsulocalize <tsulocalizing@gmail.com>
2023-09-03 11:41:09 +0900

    🐛서버 Build 오류 방지를 위한 Test 제거
    @SpringBootTest가 있을 때 EC2에서 Build 오류가 걸리는 경우가 있어 제거(임시 조치)

--- src/test/java/com/main/bbangbbang/BbangbbangApplicationTests.java
+++ src/test/java/com/main/bbangbbang/BbangbbangApplicationTests.java
@@ -3,7 +3,7 @@ package com.main.bbangbbang;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BbangbbangApplicationTests {
 
 	@Test
