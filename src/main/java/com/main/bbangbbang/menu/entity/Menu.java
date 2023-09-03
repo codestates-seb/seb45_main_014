0917a5ddf082eee3a366b4a3ac355a345af215f6
Alluringstar <zmfpemsp@naver.com>
2023-09-03 16:56:51 +0900

    ✨menu 엔티티 @Id 오류 수정

--- src/main/java/com/main/bbangbbang/menu/entity/Menu.java
+++ src/main/java/com/main/bbangbbang/menu/entity/Menu.java
@@ -2,11 +2,11 @@ package com.main.bbangbbang.menu.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table
@@ -14,8 +14,7 @@ import javax.persistence.Table;
 @Setter
 @Entity
 public class Menu {
-
-    @javax.persistence.Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id; // 필드 이름을 Id로 변경
 
