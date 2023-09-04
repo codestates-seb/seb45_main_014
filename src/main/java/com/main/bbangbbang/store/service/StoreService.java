8f9d85e90797012b9f79d3ba41ee82d8921972cf
tsulocalize <tsulocalizing@gmail.com>
2023-09-04 13:45:43 +0900

    ✨검색 종류(매장명, 지역명, 메뉴명)에 따른 StoreService Method 작성
    - findStore : id로 store 검색
    - findStoreByMenu : 해당 메뉴를 가진 Store 검색
    - findStoresByName : 매장명 like 검색
    - findStoresByRegion : regionId로 Store 검색

--- src/main/java/com/main/bbangbbang/store/service/StoreService.java
+++ src/main/java/com/main/bbangbbang/store/service/StoreService.java
@@ -3,7 +3,12 @@ package com.main.bbangbbang.store.service;
 import com.main.bbangbbang.menu.entity.Menu;
 import com.main.bbangbbang.store.entity.Store;
 import com.main.bbangbbang.store.repository.StoreRepository;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,11 +19,30 @@ public class StoreService {
 
     @Transactional(readOnly = true)
     public Store findStore(long storeId) {
-        return null;
+        Optional<Store> optionalStore = storeRepository.findById(storeId);
+        Store store = optionalStore.orElseThrow(NoSuchElementException::new);
+
+        return store;
     }
 
     @Transactional(readOnly = true)
     public Store findStoreByMenu(Menu menu) {
-        return null;
+        Long menuId = menu.getId();
+        Optional<Store> optionalStore = storeRepository.findByMenusId(menuId);
+        Store store = optionalStore.orElseThrow(NoSuchElementException::new);
+
+        return store;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Store> findStoresByName(String storeName, Pageable pageable) {
+
+        return storeRepository.findByStoreNameContaining(storeName, pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Store> findStoresByRegion(List<Long> regionIds, Pageable pageable) {
+
+        return storeRepository.findByRegionIdIn(regionIds, pageable);
     }
 }
