acf012d859af4fdb251fdc3dfdca0f851e64c779
tsulocalize <tsulocalizing@gmail.com>
2023-09-04 11:52:23 +0900

    ♻Menu Controller 리팩토링

--- src/main/java/com/main/bbangbbang/menu/controller/MenuController.java
+++ src/main/java/com/main/bbangbbang/menu/controller/MenuController.java
@@ -2,26 +2,24 @@ package com.main.bbangbbang.menu.controller;
 
 import com.main.bbangbbang.menu.dto.MenuResponseDto;
 import com.main.bbangbbang.menu.service.MenuService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 public class MenuController {
     private final MenuService menuService;
 
-    @Autowired
-    public MenuController(MenuService menuService) {
-        this.menuService = menuService;
-    }
-
-    @GetMapping("/api/stores/{store-id}/menus/{menu-id}")
-    public ResponseEntity<MenuResponseDto> getMenu(@PathVariable("store-id") Long storeId, @PathVariable("menu-id") Long menuId) {
+    @GetMapping("/stores/{store-id}/menus/{menu-id}")
+    public ResponseEntity<MenuResponseDto> getMenu(@PathVariable("store-id") Long storeId,
+                                                   @PathVariable("menu-id") Long menuId) {
         MenuResponseDto menuResponseDto = menuService.getMenuById(menuId);
+
         return ResponseEntity.ok(menuResponseDto); // 리턴 타입은 메소드 명 좌측에 항상 명시되어 있다.
     }
 }
