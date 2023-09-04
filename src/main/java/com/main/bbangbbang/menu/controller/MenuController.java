b0f6d8a3a5f3286bdd43d73d81048dca96b78947
tsulocalize <tsulocalizing@gmail.com>
2023-09-04 12:03:00 +0900

    ♻MenuMapper를 Controller에서 동작하도록 리팩토링

--- src/main/java/com/main/bbangbbang/menu/controller/MenuController.java
+++ src/main/java/com/main/bbangbbang/menu/controller/MenuController.java
@@ -1,6 +1,8 @@
 package com.main.bbangbbang.menu.controller;
 
 import com.main.bbangbbang.menu.dto.MenuResponseDto;
+import com.main.bbangbbang.menu.entity.Menu;
+import com.main.bbangbbang.menu.mapper.MenuMapper;
 import com.main.bbangbbang.menu.service.MenuService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MenuController {
     private final MenuService menuService;
+    private final MenuMapper menuMapper;
 
     @GetMapping("/stores/{store-id}/menus/{menu-id}")
     public ResponseEntity<MenuResponseDto> getMenu(@PathVariable("store-id") Long storeId,
                                                    @PathVariable("menu-id") Long menuId) {
-        MenuResponseDto menuResponseDto = menuService.getMenuById(menuId);
+        Menu menu = menuService.getMenuById(menuId);
+        MenuResponseDto menuResponseDto = new MenuResponseDto(menuMapper.menuToMenuDetailData(menu)); // responsedto를 entity로 연결
 
         return ResponseEntity.ok(menuResponseDto); // 리턴 타입은 메소드 명 좌측에 항상 명시되어 있다.
     }
--- src/main/java/com/main/bbangbbang/menu/service/MenuService.java
+++ src/main/java/com/main/bbangbbang/menu/service/MenuService.java
@@ -1,8 +1,6 @@
 package com.main.bbangbbang.menu.service;
 
-import com.main.bbangbbang.menu.dto.MenuResponseDto;
 import com.main.bbangbbang.menu.entity.Menu;
-import com.main.bbangbbang.menu.mapper.MenuMapper;
 import com.main.bbangbbang.menu.repository.MenuRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,13 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class MenuService {
     private final MenuRepository menuRepository;
-    private final MenuMapper menuMapper;
 
-    public MenuResponseDto getMenuById(Long menuId) {
+    public Menu getMenuById(Long menuId) {
         Menu menu = menuRepository.findById(menuId).get();// menurepository 꺼내 온 것
-        MenuResponseDto menuResponseDto = new MenuResponseDto(menuMapper.menuToMenuDetailData(menu)); // responsedto를 entity로 연결
 
-        return menuResponseDto; // 위에 되돌려줌
+        return menu; // 위에 되돌려줌
     }
 
 //    private static 어디에 연결을 해야 하는지 아직 미구현(Menu entity){
