8658ec59c45ad8fad620081937711470b13cc287
tsulocalize <tsulocalizing@gmail.com>
2023-09-04 11:55:19 +0900

    ♻Menu Service 리팩토링

--- src/main/java/com/main/bbangbbang/menu/service/MenuService.java
+++ src/main/java/com/main/bbangbbang/menu/service/MenuService.java
@@ -4,21 +4,19 @@ import com.main.bbangbbang.menu.dto.MenuResponseDto;
 import com.main.bbangbbang.menu.entity.Menu;
 import com.main.bbangbbang.menu.mapper.MenuMapper;
 import com.main.bbangbbang.menu.repository.MenuRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
 @Service
 public class MenuService {
     private final MenuRepository menuRepository;
     private final MenuMapper menuMapper;
-    @Autowired
-    public MenuService(MenuRepository menuRepository, MenuMapper menuMapper) {
-        this.menuRepository = menuRepository;
-        this.menuMapper = menuMapper;
-    }
 
     public MenuResponseDto getMenuById(Long menuId) {
         Menu menu = menuRepository.findById(menuId).get();// menurepository 꺼내 온 것
         MenuResponseDto menuResponseDto = menuMapper.toDto(menu); // responsedto를 entity로 연결
+
         return menuResponseDto; // 위에 되돌려줌
     }
 
