adc4f312aa63fe64efc82740a425429020761a5b
Saekyul <saekyulyang@gmail.com>
2023-09-04 14:49:25 +0900

    ✨ 마이페이지 더미 데이터 추가

---
+++ client/src/assets/data/myPageReviewData.js
@@ -0,0 +1,57 @@
+const myPageReviewData = {
+  reviews: [
+    {
+      id: 1,
+      store_name: '김가네',
+      rating: 5,
+      content: '존맛탱구리',
+      img: 'https://source.unsplash.com/random/?Baguette',
+      last_modified_at: '2022-08-16T17:30:57',
+      created_at: '2022-08-16T17:30:57',
+    },
+    {
+      id: 2,
+      store_name: '맛있는집',
+      rating: 4,
+      content: '맛있어요!',
+      img: 'https://source.unsplash.com/random/?bread',
+      last_modified_at: '2022-08-17T12:45:22',
+      created_at: '2022-08-17T12:45:22',
+    },
+    {
+      id: 3,
+      store_name: '푸드킹',
+      rating: 3,
+      content: '괜찮아요',
+      img: 'https://source.unsplash.com/random/?lunch',
+      last_modified_at: '2022-08-18T09:30:15',
+      created_at: '2022-08-18T09:30:15',
+    },
+    {
+      id: 4,
+      store_name: '맛나는맛집',
+      rating: 5,
+      content: '맛나요!',
+      img: 'https://source.unsplash.com/random/?donut',
+      last_modified_at: '2022-08-18T18:00:45',
+      created_at: '2022-08-18T18:00:45',
+    },
+    {
+      id: 5,
+      store_name: '식사왕',
+      rating: 2,
+      content: '그냥 그래요',
+      img: 'https://source.unsplash.com/random/?dinner',
+      last_modified_at: '2022-08-19T10:20:30',
+      created_at: '2022-08-19T10:20:30',
+    },
+  ],
+  pageInfo: {
+    page: 1,
+    size: 10,
+    total_elements: 20,
+    total_pages: 7,
+  },
+};
+
+export default myPageReviewData;
