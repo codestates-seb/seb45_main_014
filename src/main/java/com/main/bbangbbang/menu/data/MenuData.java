6e2899b3e0bd811ae599b9953280ffe347f7633c
tsulocalize <tsulocalizing@gmail.com>
2023-09-04 11:56:36 +0900

    ✨MenuData Class 추가
    Menu 정보를 다양하게 제공하기 위한 별도의 Data Class 생성

---
+++ src/main/java/com/main/bbangbbang/menu/data/MenuData.java
@@ -0,0 +1,15 @@
+package com.main.bbangbbang.menu.data;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.Setter;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Getter
+@Setter
+public abstract class MenuData {
+    private Long id;
+    private String menuName;
+    private Integer price;
+}
