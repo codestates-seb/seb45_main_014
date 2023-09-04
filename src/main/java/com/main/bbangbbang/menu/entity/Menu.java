58fd32957143df0abce5d8d83b4630c3cc427a04
tsulocalize <tsulocalizing@gmail.com>
2023-09-04 11:39:44 +0900

    ✨Menu Entity 유효성 검증 추가 및 생성/수정 시간 column 추가

--- src/main/java/com/main/bbangbbang/menu/entity/Menu.java
+++ src/main/java/com/main/bbangbbang/menu/entity/Menu.java
@@ -1,5 +1,11 @@
 package com.main.bbangbbang.menu.entity;
 
+import com.main.bbangbbang.store.entity.Store;
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.Column;
@@ -7,37 +13,51 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Table
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @Entity
 public class Menu {
-    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id; // 필드 이름을 Id로 변경
+    @Id
+    private Long id;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "store_id", nullable = false)
-//    private Store store; // 있던 말던 상관 없는 상태, mapper가 매핑 해주지 않음. 구현이 되어 있지 않은 기능이라 주석처리.
+    @ManyToOne
+    @JoinColumn
+    private Store store;
 
-    // dbdiagram에 menu table 보고 entity 작성한 것
-    @Column(name="menu_name", nullable = false)
+    @Column
+    @Size(max = 30)
     private String menuName;
 
     @Column
+    @Size(max = 200)
     private String menuDesc;
 
     @Column
+    @Size(max = 200)
     private String ingredient;
 
     @Column
-    private int price;
+    @Size(min = 100, max = 1000000)
+    private Integer price;
 
     @Column
-    private int stock;
+    @Size(min = 0, max = 1000)
+    private Integer stock;
 
     @Column
     private String img;
+
+    @Column
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+
+    @Column
+    @CreatedDate
+    private LocalDateTime createdAt;
 }
