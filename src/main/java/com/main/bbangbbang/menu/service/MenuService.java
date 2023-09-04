50999f04ba55ce744a82c0e2587746728345827f
tsulocalize <tsulocalizing@gmail.com>
2023-09-04 12:07:09 +0900

    ✨MenuSevice 예외처리/트랜젝션 추가

--- src/main/java/com/main/bbangbbang/menu/service/MenuService.java
+++ src/main/java/com/main/bbangbbang/menu/service/MenuService.java
@@ -2,18 +2,22 @@ package com.main.bbangbbang.menu.service;
 
 import com.main.bbangbbang.menu.entity.Menu;
 import com.main.bbangbbang.menu.repository.MenuRepository;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 @Service
 public class MenuService {
     private final MenuRepository menuRepository;
 
+    @Transactional(readOnly = true)
     public Menu getMenuById(Long menuId) {
-        Menu menu = menuRepository.findById(menuId).get();// menurepository 꺼내 온 것
+        Optional<Menu> optionalMenu = menuRepository.findById(menuId);// menurepository 꺼내 온 것
 
-        return menu; // 위에 되돌려줌
+        return optionalMenu.orElseThrow(NoSuchElementException::new); // 위에 되돌려줌
     }
 
 //    private static 어디에 연결을 해야 하는지 아직 미구현(Menu entity){
