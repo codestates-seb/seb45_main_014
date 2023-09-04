17b129e805ff3fda776b18ae814e8099d26b1c5f
tsulocalize <tsulocalizing@gmail.com>
2023-09-04 13:32:07 +0900

    ✨검색 종류(매장명, 지역명, 메뉴명)에 따른 SQL 메소드 추가

--- src/main/java/com/main/bbangbbang/store/repository/StoreRepository.java
+++ src/main/java/com/main/bbangbbang/store/repository/StoreRepository.java
@@ -1,7 +1,14 @@
 package com.main.bbangbbang.store.repository;
 
 import com.main.bbangbbang.store.entity.Store;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    Page<Store> findByStoreNameContaining(String name, Pageable pageable);
+    Page<Store> findByRegionIdIn(List<Long> regionIds, Pageable pageable);
+    Optional<Store> findByMenusId(Long menuId);
 }
