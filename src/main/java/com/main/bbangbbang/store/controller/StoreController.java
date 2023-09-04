029f1243228bffc6344f4fa733334c6aadaab9ed
tsulocalize <tsulocalizing@gmail.com>
2023-09-04 13:48:08 +0900

    ✨매장 상세페이지 메소드 작성(getStore)

--- src/main/java/com/main/bbangbbang/store/controller/StoreController.java
+++ src/main/java/com/main/bbangbbang/store/controller/StoreController.java
@@ -1,5 +1,9 @@
 package com.main.bbangbbang.store.controller;
 
+import com.main.bbangbbang.store.dto.StoreResponseDto;
+import com.main.bbangbbang.store.entity.Store;
+import com.main.bbangbbang.store.mapper.StoreMapper;
+import com.main.bbangbbang.store.service.StoreService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class StoreController { // 매장 상세 페이지
+    private final StoreService storeService;
+    private final StoreMapper storeMapper;
 
     @GetMapping("/stores/{store-id}")
     public ResponseEntity<?> getStore(@PathVariable("store-id") long storeId) { // 매장 상세 내용 (이름, 설명, 주소, 메뉴들)
-        return null;
+        Store store = storeService.findStore(storeId);
+        StoreResponseDto responseDto = new StoreResponseDto(storeMapper.storeToStoreDetailData(store));
+
+        return ResponseEntity.ok(responseDto);
     }
 
     @GetMapping("/stores/{store-id}/reviews")
