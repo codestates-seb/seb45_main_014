437e0263955d393fdaca90d60a0f8c6dd326414c
tsulocalize <tsulocalizing@gmail.com>
2023-09-05 11:24:44 +0900

    ✨Review Entity, Data 클래스 추가

---
+++ src/main/java/com/main/bbangbbang/review/data/ReviewDataWithStoreName.java
@@ -0,0 +1,13 @@
+package com.main.bbangbbang.review.data;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.Setter;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Getter
+@Setter
+public class ReviewDataWithStoreName extends ReviewData {
+    private String storeName;
+}
