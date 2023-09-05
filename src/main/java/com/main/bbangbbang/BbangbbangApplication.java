620680e0f1dbff8921959428efeea63d382140ad
kimhaming <kimhamin83@gmail.com>
2023-09-05 11:14:16 +0900

    ✅ Hello, World!

--- src/main/java/com/main/bbangbbang/BbangbbangApplication.java
+++ src/main/java/com/main/bbangbbang/BbangbbangApplication.java
@@ -10,6 +10,6 @@ public class BbangbbangApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BbangbbangApplication.class, args);
+		System.out.println("Hello, World!");
 	}
-
 }
