bd45e0b37cefb72ff20347feae5c4b9b849ffb20
nuyhv <volumefd@gmail.com>
2023-09-06 23:18:19 +0900

    💄 Main overflow 속성 제거

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
     <>
       <GlobalStyle />
       <Header />
-      <main className="w-full mx-auto overflow-auto min-h-screen">
+      <main className="w-full mx-auto min-h-screen">
         <ScrollButton />
         <Suspense fallback={<LoadingSpinner />}>
           <Routes>
