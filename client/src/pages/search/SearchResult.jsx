1dc67ba44b2a75d638b20940761682add6d9572a
nuyhv <volumefd@gmail.com>
2023-09-07 20:53:31 +0900

    ✨ 검색결과가 없는 경우의 렌더링 케이스 추가

--- client/src/pages/search/SearchResult.jsx
+++ client/src/pages/search/SearchResult.jsx
@@ -3,9 +3,11 @@ import { useState, useEffect } from 'react';
 import StoreCard from '../../assets/StoreCard.jsx';
 import axios from 'axios';
 import { useLocation, useSearchParams } from 'react-router-dom';
+import { ReactComponent as Magnifier } from '../../assets/images/magnifier.svg';
 
 const Wrapper = styled.section`
   max-width: 650px;
+  height: 100%;
   margin: 0 auto 0 auto;
   padding-top: 30px;
   padding-bottom: 30px;
@@ -17,6 +19,31 @@ const DataContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   flex-wrap: wrap;
+`;
+
+const NoResult = styled.div`
+  display: flex;
+  padding: 40px 0;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 600px;
+  font-size: 20px;
+  font-weight: 500;
+  font-family:
+    system-ui,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    Oxygen,
+    Ubuntu,
+    Cantarell,
+    'Open Sans',
+    'Helvetica Neue',
+    sans-serif;
+  color: rgb(181, 181, 181);
 `;
 
 const SearchResult = () => {
@@ -37,56 +64,24 @@ const SearchResult = () => {
     }
   }, [location, API]);
 
-  // useEffect(() => {
-  //   switch (searchFilter) {
-  //     case 'store':
-  //       if (searchQuery) {
-  //         const filteredData = storeData.filter((data) =>
-  //           data.store_name.includes(searchQuery),
-  //         );
-  //         setData(filteredData);
-  //         setResultQuery(searchQuery);
-  //         console.log(`키워드는 ${resultQuery} 입니다.`);
-  //       } else {
-  //         setData(storeData);
-  //       }
-  //       break;
-
-  //     case 'region':
-  //       if (searchQuery) {
-  //         const filteredData = storeData.filter((data) =>
-  //           data.region_name.includes(searchQuery),
-  //         );
-  //         setData(filteredData);
-  //         setResultQuery(searchQuery);
-  //         console.log(`키워드는 ${resultQuery} 입니다.`);
-  //       } else {
-  //         setData(storeData);
-  //       }
-  //       break;
-
-  //     case 'menu':
-  //       // 'menu'에 대한 로직
-  //       break;
-
-  //     default:
-  //       // 기본 동작을 정의
-  //       break;
-  //   }
-  // }, [location, resultQuery, searchQuery, searchFilter]);
-
   // query string에서 search_keyword를 가져옴
   const [searchParams] = useSearchParams();
   const query = searchParams.get('search_keyword');
 
   return (
     <Wrapper>
-      <div className="text-[20px] pb-3">
+      <div className="text-[24px] pb-3">
         <span className="flex justify-center">
           <p className="text-[#debe8f] font-semibold">{`'${query}'`}</p>
           <p>에 대한 검색결과</p>
         </span>
       </div>
+      {data.length === 0 && (
+        <NoResult>
+          <Magnifier />
+          <p className="pt-8">검색된 결과가 없습니다.</p>
+        </NoResult>
+      )}
       <DataContainer>
         {data.map((store, idx) => (
           <StoreCard key={idx} store={store} />
