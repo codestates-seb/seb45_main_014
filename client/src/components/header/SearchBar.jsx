c8f12297cd93467af3437a82c2eb59830a0cc6f6
nuyhv <volumefd@gmail.com>
2023-09-07 20:52:41 +0900

    🔥 Console.log 제거

--- client/src/components/header/SearchBar.jsx
+++ client/src/components/header/SearchBar.jsx
@@ -61,7 +61,6 @@ const SearchBar = () => {
   const handleSearchQuery = (e) => {
     const query = e.target.value;
     setSearchQuery(query);
-    console.log(`searchQuery 값은 ${searchQuery}`);
   };
 
   const searchSubmitHandler = (e) => {
