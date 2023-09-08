ccca3d48f10c7f62fbe4e0118c87b72d3c005eeb
JongHoSke <astra9706@naver.com>
2023-09-08 10:34:02 +0900

    :sparkles: 매장 상세 페이지 UI 변경 및 탭 클릭시 해당 요소로 스크롤 이동

--- client/src/pages/Store.jsx
+++ client/src/pages/Store.jsx
@@ -1,6 +1,6 @@
 import ShopInfo from '../components/store/ShopInfo.jsx';
 import { useParams, Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import MenuTab from '../components/store/MenuTab.jsx';
 import StoreReviewTab from '../components/store/StoreReviewTab.jsx';
 import axios from 'axios';
@@ -11,6 +11,8 @@ const Store = () => {
   const [currentTab, setCurrentTab] = useState('메뉴');
   const [storeData, setStoreData] = useState(null);
   const [reviewData, setReviewData] = useState([]);
+  const menuRef = useRef(null);
+  const reviewRef = useRef(null);
   const apiUrl = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
@@ -40,32 +42,47 @@ const Store = () => {
     return <LoadingSpinner />;
   }
 
+  const handleTabClick = (tab) => {
+    setCurrentTab(tab);
+    if (tab === '메뉴') {
+      menuRef.current.scrollIntoView({ behavior: 'smooth' });
+    } else if (tab === '후기') {
+      reviewRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="flex flex-col relative">
       <ShopInfo store={storeData} />
       <ul className="flex justify-center text-center w-[1070px] mx-auto border-b mb-1 sticky top-[65px] bg-white z-10">
         <li className="w-full hover:bg-[#ccc] border-r py-3">
-          <Link
-            to="#매장"
-            className="block w-full"
+          <a
+            href="#메뉴"
+            className="block w-full cursor-pointer"
             onClick={() => setCurrentTab('메뉴')}
           >
             메뉴 ({storeData.menus.length})
-          </Link>
+          </a>
         </li>
         <li className="w-full hover:bg-[#ccc] py-3">
-          <Link
-            to="#매장"
-            className="block w-full"
+          <a
+            href="#후기"
+            className="block w-full cursor-pointer"
             onClick={() => setCurrentTab('후기')}
           >
             후기 ({reviewData.length})
-          </Link>
+          </a>
         </li>
       </ul>
-      <div className="flex mx-auto">
-        {currentTab === '메뉴' && <MenuTab menuData={storeData.menus} />}
-        {currentTab === '후기' && <StoreReviewTab reviewData={reviewData} />}
+      <div className="flex flex-col mx-auto">
+        <div id="메뉴" ref={menuRef}>
+          메뉴
+          <MenuTab menuData={storeData.menus} />
+        </div>
+        <div id="후기" ref={reviewRef}>
+          후기
+          <StoreReviewTab reviewData={reviewData} />
+        </div>
       </div>
     </div>
   );
