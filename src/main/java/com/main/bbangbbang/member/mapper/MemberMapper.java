73ee0850792c46b6e91ef9d50aca64f72a7c373b
kimhaming <kimhamin83@gmail.com>
2023-09-09 02:22:14 +0900

    :sparkles:  MemberMapper 수정

--- src/main/java/com/main/bbangbbang/member/mapper/MemberMapper.java
+++ src/main/java/com/main/bbangbbang/member/mapper/MemberMapper.java
@@ -1,6 +1,6 @@
 package com.main.bbangbbang.member.mapper;
 
-import com.main.bbangbbang.member.dto.requestDto.AuthLoginDto;
+import com.main.bbangbbang.member.dto.requestDto.MemberPostDto;
 import com.main.bbangbbang.member.dto.requestDto.MemberPatchDto;
 import com.main.bbangbbang.member.dto.responseDto.MemberResponseDto;
 import com.main.bbangbbang.member.entity.Member;
@@ -9,7 +9,7 @@ import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MemberMapper {
-    Member authLoginDtoToMember(AuthLoginDto authLoginDto);
+    Member memberPostDtoToMember(MemberPostDto memberPostDto);
     Member memberPatchDtoToMember(MemberPatchDto memberPatchDto);
     MemberResponseDto memberToMemberResponseDto(Member member);
 }
