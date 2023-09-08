c23b9aef226fbd31acab126568d5f55d7b796efe
tsulocalize <tsulocalizing@gmail.com>
2023-09-08 13:44:44 +0900

    ✨Store Entity 및 DetailData에 placeId 추가

--- src/main/java/com/main/bbangbbang/store/data/StoreDetailData.java
+++ src/main/java/com/main/bbangbbang/store/data/StoreDetailData.java
@@ -1,14 +1,18 @@
 package com.main.bbangbbang.store.data;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.main.bbangbbang.menu.data.MenuDetailData;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Getter
 @Setter
 public class StoreDetailData extends StoreData {
     private String address;
+    private Long placeId;
     private String introduction;
     private List<MenuDetailData> menus;
 }
