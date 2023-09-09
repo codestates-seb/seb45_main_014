2ea45b2407986a58ed79e50ae5dddb6dc83c2020
Saekyul <saekyulyang@gmail.com>
2023-09-09 18:13:29 +0900

    🔥 미사용 import 삭제

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,9 @@ import Header from './components/header/Header.jsx';
 import { createGlobalStyle } from 'styled-components';
 import GmarketSans from './assets/fonts/GmarketSansTTFMedium.ttf';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import { lazy, Suspense, useState, useEffect } from 'react';
+import { lazy, Suspense } from 'react';
 import LoadingSpinner from './components/Loading.jsx';
 import ScrollButton from './assets/buttons/ScrollButton.jsx';
-import saveAuthorizationToken from './SaveAuthorizationToken.jsx';
-import axios from 'axios';
 
 const NotFound = lazy(() => import('./pages/NotFound.jsx'));
 const MainPage = lazy(() => import('./pages/main/MainPage.jsx'));
@@ -17,30 +15,6 @@ const Store = lazy(() => import('./pages/Store.jsx'));
 const EditProfile = lazy(() => import('./components/myPage/EditProfile.jsx'));
 const SearchResult = lazy(() => import('./pages/search/SearchResult.jsx'));
 const Cart = lazy(() => import('./pages/cart/Cart.jsx'));
-
-const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'GmarketSans';
-    src: url(${GmarketSans}) format('truetype');
-    font-weight: 300;
-    font-style: normal;
-  }
-  * {
-    box-sizing: border-box;
-  }
-  html, body {
-    font-family: 'GmarketSans', sans-serif;
-    height: 100%;
-    padding: 0;
-    margin: 0;
-  }
-  body {
-    padding-top: 65px;
-  }
-  ul, li {
-    list-style-type: none;
-  }
-  `;
 
 function App() {
   const location = useLocation();
@@ -70,3 +44,27 @@ function App() {
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'GmarketSans';
+    src: url(${GmarketSans}) format('truetype');
+    font-weight: 300;
+    font-style: normal;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  html, body {
+    font-family: 'GmarketSans', sans-serif;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
+  body {
+    padding-top: 65px;
+  }
+  ul, li {
+    list-style-type: none;
+  }
+  `;
