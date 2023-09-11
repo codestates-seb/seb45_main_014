5248d11b02aaf11d6bd630a80eb2d51bf3396cc5
tsulocalize <tsulocalizing@gmail.com>
2023-09-11 16:49:29 +0900

    ✨ReviewController 멤버 확인 절차 추가
    - NO_ACCESS exceptionCode 추가

--- src/main/java/com/main/bbangbbang/exception/ExceptionCode.java
+++ src/main/java/com/main/bbangbbang/exception/ExceptionCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ExceptionCode {
-    NOT_SAME_STORE(900, "다른 매장에서 주문 중입니다.");
+    NOT_SAME_STORE(900, "다른 매장에서 주문 중입니다."),
+    NO_ACCESS(901, "권한이 없습니다.");
 
     private final int status;
     private final String message;
