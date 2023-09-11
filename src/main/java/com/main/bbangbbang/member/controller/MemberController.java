067649f0f5b8e0603573068aa40556072f8299af
kimhaming <kimhamin83@gmail.com>
2023-09-11 22:48:59 +0900

    :bug: 회원 정보 수정 메서드 내부 코드 수정하여 수정 안되는 오류 해결

--- src/main/java/com/main/bbangbbang/member/controller/MemberController.java
+++ src/main/java/com/main/bbangbbang/member/controller/MemberController.java
@@ -33,40 +33,22 @@ public class MemberController {
                                       Authentication authentication) {
         try {
             String email = authentication.getPrincipal().toString();
-            // MemberPatchDto를 사용하여 회원 정보를 수정하고, 수정된 회원 정보를 MemberResponseDto로 변환// Authroization 헤더에 토큰 담으면 -> JwtVerificationFilter에서 토큰 검증 할 때
-            //            필터가 토큰 확인해서 토큰이 유효하면 신분증을 시큐리티컨텍스트홀더에 넣어놓는다.
-            //            -> 컨트롤러에서 시큐리티컨텍스트홀더에서 신분증 꺼내서 사용할 수 있다.
-            //                    -> authentication 신분증에서 principal(주체) 꺼내서 사용할 수 있다.
-            // 여기서 authentication의 역할은 끝 !
-            //            -> 이제부터는 서비스에서 principal(주체)를 사용할 수 있다.
-            //            -> 서비스에서 principal(주체)를 사용할 수 있게 하려면
-            //            -> 서비스에 principal(주체)를 전달해줘야 한다.
-            //             SetAuthentication(authentication)
-            // SecurityContextHolder는 필터에서 자격증명이 된 유저 정보를 저장한다.
-            //            -> 필터에서 자격증명이 된 유저 정보를 저장할 때 사용하는 것이 SecurityContextHolder
-            // 필터는 언제 이루어지는가? -> 요청이 들어올 때마다 필터가 실행된다.
-            // 필터가 10종류 정도 있는데 그 중에 하나가 JwtVerificationFilter
-            // Oauth2LoginAuthenticationFilter -> OAuth2 로그인을 처리하는 필터
-            // JwtVerificationFilter -> JWT 토큰을 검증하는 필터
-            // Request -> Filter -> Controller -> Service -> Repository -> DB -> Repository -> Service -> Controller -> Filter -> Response
-            // 10종류 중 로그인을 오어스로그인으로 하게 되면
-
             Member member = memberService.findMember(email);
 
-//            if (member == null) {
-//
-//                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//            }
-//
-//            if (memberPatchDto.getNickname() != null) {
-//                member.setNickname(memberPatchDto.getNickname());
-//            }
-//
-//            if (memberPatchDto.getImg() != null) {
-//                member.setImg(memberPatchDto.getImg());
-//            }
+            if (member == null) {
 
-            memberService.updateMember(member);
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            if (memberPatchDto.getNickname() != null) {
+                member.setNickname(memberPatchDto.getNickname());
+            }
+
+            if (memberPatchDto.getImg() != null) {
+                member.setImg(memberPatchDto.getImg());
+            }
+
+            member = memberService.updateMember(member);
             MemberResponseDto responseDto = memberMapper.memberToMemberResponseDto(member);
 
             return new ResponseEntity<>(responseDto, HttpStatus.OK);
