20f26f8a05bde5ab30798e018746953e32f0b15a
Saekyul <saekyulyang@gmail.com>
2023-09-11 10:49:40 +0900

    ✨ 마이페이지에서 데이터를 렌더링하도록 수정

--- client/src/pages/MyPage.jsx
+++ client/src/pages/MyPage.jsx
@@ -41,6 +41,13 @@ const MyPage = () => {
   const [currentTab, setCurrentTab] = useState('리뷰 관리');
   const { isLoggedIn, accessToken } = useAuthStore((state) => state);
   const [member, setMember] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+
+  const [reviewCount, setReviewCount] = useState(0);
+  const [orderCount, setOrderCount] = useState(0);
+  const [favoriteCount, setFavoriteCount] = useState(0);
 
   useEffect(() => {
     if (!isLoggedIn || !accessToken) {
@@ -48,6 +55,7 @@ const MyPage = () => {
       return;
     }
 
+    // 회원 정보 가져오기
     const getMember = async () => {
       try {
         const response = await axios.get(
@@ -64,6 +72,60 @@ const MyPage = () => {
       }
     };
     getMember();
+
+    // 리뷰 가져오기
+    const getReviews = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/api/reviews`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          },
+        );
+        setReviews(response.data);
+        setReviewCount(response.data.length);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    // 주문 내역 가져오기
+    const getOrders = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/api/members/orders`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          },
+        );
+        setOrders(response.data);
+        setOrderCount(response.data.length);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    // 즐겨찾기 가져오기
+    const getFavorites = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/api/members/favorites`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          },
+        );
+        setFavorites(response.data);
+        setFavoriteCount(response.data.length);
+      } catch (error) {
+        console.error(error);
+      }
+    };
   }, [isLoggedIn, accessToken]);
 
   return (
@@ -83,30 +145,27 @@ const MyPage = () => {
           </Button>
         </div>
       </div>
-      {/* <div className="w-full h-48 border-zinc-700 border-2 my-6">
-        유저 이미지
-      </div> */}
       <TabContainer className="mb-5">
         <li className="w-full">
           <Link to="#review" onClick={() => setCurrentTab('리뷰 관리')}>
-            리뷰 관리
+            리뷰 관리 ({reviewCount})
           </Link>
         </li>
         <li className="w-full">
           <Link to="#order" onClick={() => setCurrentTab('주문 내역')}>
-            주문 내역
+            주문 내역 ({orderCount})
           </Link>
         </li>
         <li className="w-full">
           <Link to="#favorite" onClick={() => setCurrentTab('즐겨찾기')}>
-            즐겨찾기
+            즐겨찾기 ({favoriteCount})
           </Link>
         </li>
       </TabContainer>
       <div className="flex justify-center">
-        {currentTab === '리뷰 관리' && <Reviews />}
-        {currentTab === '주문 내역' && <Orders />}
-        {currentTab === '즐겨찾기' && <Favorites />}
+        {currentTab === '리뷰 관리' && <Reviews data={reviews} />}
+        {currentTab === '주문 내역' && <Orders data={orders} />}
+        {currentTab === '즐겨찾기' && <Favorites data={favorites} />}
       </div>
     </div>
   );
