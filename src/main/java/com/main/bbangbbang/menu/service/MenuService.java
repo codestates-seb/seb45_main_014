7cde3b92b7b0091e04eec7b3e022df62c03baae3
tsulocalize <tsulocalizing@gmail.com>
2023-09-13 13:25:36 +0900

    ✨재고 부족 Exception에 대해 메뉴, 수량을 명시하여 return하도록 수정

--- src/main/java/com/main/bbangbbang/menu/service/MenuService.java
+++ src/main/java/com/main/bbangbbang/menu/service/MenuService.java
@@ -1,5 +1,7 @@
 package com.main.bbangbbang.menu.service;
 
+import com.main.bbangbbang.exception.BusinessLogicException;
+import com.main.bbangbbang.exception.ExceptionCode;
 import com.main.bbangbbang.menu.entity.Menu;
 import com.main.bbangbbang.menu.repository.MenuRepository;
 import java.util.List;
@@ -31,7 +33,8 @@ public class MenuService {
     public void calculateStock(Menu menu, Integer quantity) throws RuntimeException {
         Integer currentStock = menu.getStock();
         if (currentStock < quantity) {
-            throw new RuntimeException(String.format("No Stocks for quantity: %d",quantity));
+            throw new BusinessLogicException(ExceptionCode.OUT_OF_STOCK,
+                    String.format("메뉴명 : %s\n재고 수량:%d\n주문 수량: %d", menu.getMenuName(), currentStock, quantity));
         }
         menu.setStock(currentStock - quantity);
     }
