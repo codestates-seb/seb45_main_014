2336c32a060c85b9d9ef46e8219d3b6d1eb4e00f
nuyhv <volumefd@gmail.com>
2023-09-13 16:22:17 +0900

    ✨ 장바구니 관련 API 생성

---
+++ client/src/api/cart.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { useAuthStore } from '../store/store';
+
+export const useCartApi = () => {
+  const { accessToken } = useAuthStore();
+  const API = process.env.REACT_APP_API_URL;
+  const config = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+  // 장바구니 GET 요청
+  const fetchCart = async () => {
+    try {
+      const response = await axios.get(`${API}/api/cart`, config);
+      return response.data.order;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 장바구니에 아이템 추가 POST 요청 (해주세요 ㅎㅎ)
+  const addCart = async (data) => {
+    try {
+      // POST 해서 볶아먹을 내용
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 장바구니에서 아이템 삭제 DELETE 요청
+  const deleteCart = async (data) => {
+    try {
+      await axios.delete(`${API}/api/cart`, {
+        ...config,
+        data: data,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 장바구니에 체크된 아이템 주문 POST 요청
+  const orderCart = async (pickupTime) => {
+    try {
+      await axios.post(
+        `${API}/api/cart?pickup_time=${pickupTime}`,
+        null,
+        config,
+      );
+      // 성공한 경우
+      alert('주문이 완료되었습니다.');
+    } catch (error) {
+      console.error('주문 중 오류가 발생했습니다.', error);
+      alert('주문 중 오류가 발생했습니다.');
+    }
+  };
+
+  return { fetchCart, addCart, deleteCart, orderCart };
+};
