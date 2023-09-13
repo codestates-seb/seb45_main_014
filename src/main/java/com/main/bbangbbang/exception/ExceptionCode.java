df979f47240c98c7ccdd73cda5a04e15e865aff2
kimhaming <kimhamin83@gmail.com>
2023-09-13 11:06:58 +0900

    :sparkles: ExceptionCode에 토큰 만료 시/ 로그인 안되었을 시에 대한 상태 코드 추가

--- src/main/java/com/main/bbangbbang/exception/ExceptionCode.java
+++ src/main/java/com/main/bbangbbang/exception/ExceptionCode.java
@@ -11,7 +11,10 @@ public enum ExceptionCode {
     NO_ACTIVE_ORDER(902, "장바구니가 없습니다."),
     MANY_ACTIVE_ORDER(903, "장바구니가 너무 많습니다."),
     NO_MEMBER(904, "멤버가 존재하지 않습니다."),
-    NO_ITEM(905, "해당 아이템이 없습니다.");
+    NO_ITEM(905, "해당 아이템이 없습니다."),
+
+    JWT_TOKEN_EXPIRED(404,"토큰이 만료되었습니다"),
+    UNAUTHENTICATED_USER(908, "인증되지 않은 사용자입니다. 로그인이 필요합니다.");
 
     private final int status;
     private final String message;
