6b804db682fe5edebcc815e5d71ee670e6d0a402
tsulocalize <tsulocalizing@gmail.com>
2023-09-13 13:24:56 +0900

    ✨BusinessException에 상세 내용을 기입할 수 있는 기능 추가

--- src/main/java/com/main/bbangbbang/exception/ExceptionCode.java
+++ src/main/java/com/main/bbangbbang/exception/ExceptionCode.java
@@ -11,7 +11,8 @@ public enum ExceptionCode {
     NO_ACTIVE_ORDER(902, "장바구니가 없습니다."),
     MANY_ACTIVE_ORDER(903, "장바구니가 너무 많습니다."),
     NO_MEMBER(904, "멤버가 존재하지 않습니다."),
-    NO_ITEM(905, "해당 아이템이 없습니다.");
+    NO_ITEM(905, "해당 아이템이 없습니다."),
+    OUT_OF_STOCK(906, "재고가 부족합니다:");
 
     private final int status;
     private final String message;
