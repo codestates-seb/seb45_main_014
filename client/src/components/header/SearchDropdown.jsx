5ef6c0f394e7ae516350e6645625b55cf1055744
nuyhv <volumefd@gmail.com>
2023-09-15 12:53:31 +0900

    ✨ SearchDropdown에 최근 검색어 렌더링

--- client/src/components/header/SearchDropdown.jsx
+++ client/src/components/header/SearchDropdown.jsx
@@ -3,15 +3,15 @@ import { styled } from 'styled-components';
 
 const Dropdown = styled.div`
   position: absolute;
-  top: 120%;
-  right: 1;
-  width: calc(50% + 70px);
+  top: 110%;
+  width: 100%;
   height: 300px;
   background-color: white;
-  border: 2px solid #f9d4b1;
+  border: 2px solid #debe8f;
   border-radius: 10px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
   z-index: 102;
+  overflow: hidden;
 `;
 
 const SearchDropdown = () => {
@@ -43,9 +43,29 @@ const SearchDropdown = () => {
     };
   }, [isMenuOpen]);
 
+  const recentSearches = JSON.parse(localStorage.getItem('recentSearches'));
+
   return (
     <Dropdown isOpen={isMenuOpen} ref={dropdownRef}>
-      검색어 리스트 오는곳
+      <h2 className="bg-[#debe8f] px-2 py-1 text-white font-semibold text-sm">
+        최근 검색어
+      </h2>
+      <ul className="text-sm">
+        {!recentSearches ? (
+          <li className="pt-4 text-center text-lg text-gray-300">
+            최근 검색어가 없습니다.
+          </li>
+        ) : (
+          recentSearches.map((term, index) => (
+            <li
+              key={index}
+              className="text-gray-400 hover:bg-slate-100 py-1 px-2"
+            >
+              {term}
+            </li>
+          ))
+        )}
+      </ul>
     </Dropdown>
   );
 };
