bbaf23804c997fc999be6ff6aa7ef38f9f12f9f1
kimhaming <kimhamin83@gmail.com>
2023-09-16 22:07:28 +0900

    :white_check_mark: 리다이렉트 url 배포주소로 변경하여 테스트

--- src/main/java/com/main/bbangbbang/auth/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/main/bbangbbang/auth/handler/OAuth2LoginSuccessHandler.java
@@ -105,9 +105,9 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
         return UriComponentsBuilder
                 .newInstance()
                 .scheme("http")
-//                .host("bbangorder.s3-website.ap-northeast-2.amazonaws.com")
-                .host("localhost")
-                .port(3000) // 추후 포트번호 변경 시 작성
+                .host("bbangorder.store")
+//                .host("localhost")
+//                .port(3000) // 추후 포트번호 변경 시 작성
                 .path("/auth/google")
                 .queryParams(queryParams)
                 .build()
