fc707e4a0f015500a4b388a3c003077dfcb24cfc
Saekyul <saekyulyang@gmail.com>
2023-09-17 14:54:06 +0900

    ✨ 기존 이미지를 프리뷰로 가져오기

--- client/src/components/myPage/ImageUploadModal.jsx
+++ client/src/components/myPage/ImageUploadModal.jsx
@@ -5,7 +5,7 @@ import Button from '../../assets/buttons/Button.jsx';
 import { CancelButton } from './EditProfile.jsx';
 import { useAuthStore } from '../../store/store.js';
 
-const ImageUploadModal = ({ onClose }) => {
+const ImageUploadModal = ({ onClose, memberImage }) => {
   const inputRef = useRef(null);
 
   const [preview, setPreview] = useState(null);
@@ -60,8 +60,10 @@ const ImageUploadModal = ({ onClose }) => {
           onSubmit={handleUpload}
         >
           <div className="w-64 h-64 border-2 border-black-100">
-            {preview && (
+            {preview ? (
               <img className="w-full h-full" src={preview} alt="미리보기" />
+            ) : (
+              <img className="w-full" src={memberImage} alt="유저 이미지" />
             )}
           </div>
           <input
