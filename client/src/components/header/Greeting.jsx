dd0a2f75bf53b8a4afe131848d98808d18bc4eb0
Saekyul <saekyulyang@gmail.com>
2023-09-17 22:35:34 +0900

    🐛 토큰 불러오지 못하는 현상 해결하기 위해 주석 처리

--- client/src/components/header/Greeting.jsx
+++ client/src/components/header/Greeting.jsx
@@ -24,27 +24,27 @@ const Greeting = () => {
   }, [accessToken]);
 
   // nickname이 없을 경우 엑세스 토큰 재요청
-  useEffect(() => {
-    if (!nickname) {
-      axios
-        .post(
-          `${process.env.REACT_APP_API_URL}/api/token/refresh`,
-          {},
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-              Refresh: `Bearer ${refreshToken}`,
-            },
-          },
-        )
-        .then((res) => {
-          login(res.data.accessToken);
-        })
-        .catch((err) => {
-          console.log('리프레시 토큰 에러', err);
-        });
-    }
-  }, [accessToken, login, nickname, refreshToken]);
+  // useEffect(() => {
+  //   if (!nickname) {
+  //     axios
+  //       .post(
+  //         `${process.env.REACT_APP_API_URL}/api/token/refresh`,
+  //         {},
+  //         {
+  //           headers: {
+  //             Authorization: `Bearer ${accessToken}`,
+  //             Refresh: `Bearer ${refreshToken}`,
+  //           },
+  //         },
+  //       )
+  //       .then((res) => {
+  //         login(res.data.accessToken);
+  //       })
+  //       .catch((err) => {
+  //         console.log('리프레시 토큰 에러', err);
+  //       });
+  //   }
+  // }, [accessToken, login, nickname, refreshToken]);
 
   return (
     <div>
